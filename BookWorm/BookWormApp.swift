a795af3cdcce08c05b2909f46d889c218edcf12c
Trance <xp46290@gmail.com>
2022-11-21 20:39:23 -0500

    Initial Commit

---
+++ BookWorm/BookWormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookWormApp.swift
+//  BookWorm
+//
+//  Created by Mark Trance on 11/21/22.
+//
+
+import SwiftUI
+
+@main
+struct BookWormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
