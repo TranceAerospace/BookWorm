1a6604e5b15b8261eeed8128c228f900c738ddc5
Trance <xp46290@gmail.com>
2022-11-28 21:56:02 -0500

    Project Setup

---
+++ BookWorm/DataController.swift
@@ -0,0 +1,23 @@
+//
+//  DataController.swift
+//  BookWorm
+//
+//  Created by Mark Trance on 11/21/22.
+//
+
+import Foundation
+import CoreData
+
+class DataController: ObservableObject {
+    let container = NSPersistentContainer(name: "BookWorm")
+    
+    
+    init() {
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                print("Core Data failed to load: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+}
